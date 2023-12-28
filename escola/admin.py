c999b98156fe70aff567ee532a6811cf89375826
PedroHRodrigues1 <122585029+PedroHRodrigues1@users.noreply.github.com>
2023-12-28 18:19:31 -0300

    Add files via upload

---
+++ escola/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from escola.models import Aluno, Curso, Matricula
+
+
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+admin.site.register(Aluno, Alunos)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',) 
+
+admin.site.register(Curso, Cursos)
+
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id',)
+
+
+admin.site.register(Matricula, Matriculas)
---
+++ escola/serializer.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'name', 'rg', 'cpf', 'data_nascimento']
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
+
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.name')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
+        
+    
